1885cfe403382e426166459cb7045fdcba4dc577
Trace Thompson <tracethmpsn@gmail.com>
2015-04-09 10:53:24 -0700

    [cleanup] Fixed template names & removed unnecessary functions

--- sketch.js
+++ sketch.js
@@ -1,27 +1,21 @@
 if (Meteor.isClient) {
   var cv; //canvas variable
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.canvasDisplay.helpers({
+    //add helpers here
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.canvasDisplay.events({
+    //add event listeners here
   });
 
-  Template.hello.onRendered(function() {
+  Template.canvasDisplay.onRendered(function() {
     cv = $('.canvas').sketch();
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
