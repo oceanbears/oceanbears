e9d6723eafa7a66a140376f99e6659095b377740
Bryan Lee <pygmize.me@gmail.com>
2015-04-10 17:09:16 -0700

    [feature] Added user count display
    Server has userCollection to keep track of users currently connected
    Client display has a template that displays the current number of users online

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 // adds points database
 var points = new Meteor.Collection('pointsCollection');
+var users = new Meteor.Collection('usersCollection');
 
 Meteor.publish('pointsSubscription', function() {
   return points.find();
 })
 
+Meteor.publish('usersSubscription', function() {
+  return users.find();
+})
 
+//Add user to the users collection when their socket opens (http://docs.meteor.com/#/full/meteor_onconnection)
+Meteor.onConnection(function(conn) {
+  users.insert({ user: conn.id });
+  var conn = conn;
+  conn.onClose(function() {
+    //remove that user from the collection when it disconnects
+    users.remove({ user: conn.id });
+  });
+});
