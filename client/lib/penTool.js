d4095892d0da8f3d1c0c250144e036e780f32c80
Bryan Lee <pygmize.me@gmail.com>
2015-04-15 11:59:52 -0700

    [Feature] Pen tool draws lines instead of circles
    Changed svg draw to put in lines instead of circles
    Changed references from radius to size instead

--- client/lib/penTool.js
+++ client/lib/penTool.js
@@ -1,9 +1,11 @@
 //This creates the tools object if it does not already exist;
 Meteor.tools = Meteor.tools || {};
 
-var color;
-var radius = 10;
-var startPt;
+var color = 'black';
+var size = 10;
+//StartX and StartY are the from coordinates; when drawing, a line is created from the start coordinates to the current mouse position
+var startX;
+var startY;
 
 //This function is called when a new color is submitted and resets the form.
 Meteor.tools.getColor = function() {
@@ -12,13 +14,13 @@ Meteor.tools.getColor = function() {
   return false;
 };
 
-//This function is called when a new radius dimension is submitted. 
-Meteor.tools.getRadius = function() {
-  var radiusValue = parseInt(document.getElementById("radius").value);
-  if (radiusValue < 31 && radiusValue > 4) {
-    radius = radiusValue;
+//This function is called when a new size dimension is submitted. 
+Meteor.tools.getSize = function() {
+  var sizeValue = parseInt(document.getElementById("size").value);
+  if (sizeValue < 31 && sizeValue > 4) {
+    size = sizeValue;
   } else {
-    radius = 10;
+    size = 10;
   }
   return false;
 };
@@ -29,12 +31,30 @@ Meteor.tools.Pen = function() {
   //The markPoint function is called whenever the mouse moved. This is the function
   //that will be used to create the different behaviors of the different tools.
   this.markPoint = function() {
+    //Insert a line from the start coordinates to the current mouse position. 
     var offset = $('.canvasView').offset();
-    points.insert({
-      x: (event.pageX - offset.left),
-      y: (event.pageY - offset.top),
-      color: color, //Added color value. Color is set through the input form. 
-      radius: radius //Added radius value. Radius is set through the input form.
-    });
+    var currX = event.pageX - offset.left;
+    var currY = event.pageY - offset.top;
+    if (startX === undefined && startY === undefined) {
+      startX = currX;
+      startY = currY;
+    } else {
+      points.insert({
+        x1: startX,
+        y1: startY,
+        x2: currX,
+        y2: currY,
+        color: color, //Added color value. Color is set through the input form. 
+        size: size //Added size value. Radius is set through the input form.
+      });
+      //Prepare startX and startY for the next line or reset it if user is done drawing
+      if (Session.get('draw')) {
+        startX = currX;
+        startY = currY;
+      } else {
+        startX = undefined;
+        startY = undefined;
+      }
+    }
   };
 };
